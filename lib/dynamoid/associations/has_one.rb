faedd8031c62f77781d04a9ce97147563c5c0ab8
Anantha Kumaran <ananthakumaran@gmail.com>
2012-03-23 21:01:38 +0530

    extract duplicate code into single association

--- lib/dynamoid/associations/has_one.rb
+++ lib/dynamoid/associations/has_one.rb
@@ -4,39 +4,9 @@ module Dynamoid #:nodoc:
   # The HasOne association.
   module Associations
     class HasOne
-      include Dynamoid::Associations::Association
+      include Association
+      include SingleAssociation
       
-      # Is this object equal to the association's target?
-      #
-      # @return [Boolean] true/false
-      #
-      # @since 0.2.0
-      def ==(other)
-        target == other
-      end
-
-      # Delegate methods we don't find directly to the target.
-      #
-      # @since 0.2.0
-      def method_missing(method, *args)
-        if target.respond_to?(method)
-          target.send(method, *args)
-        else
-          super
-        end
-      end
-      
-      private
-      
-      # Find the target of the has_one association.
-      #
-      # @return [Dynamoid::Document] the found target (or nil if nothing)
-      #
-      # @since 0.2.0
-      def target
-        records.first
-      end
-
       # Find the target association, always a :belongs_to association. Uses either options[:inverse_of] or the source class name 
       # and default parsing to return the most likely name for the target association.
       #
