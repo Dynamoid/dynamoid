1cd0bb242467e9c43b4eb2e2a9282b4f73531daf
Lou <louiscuny@gmail.com>
2013-09-02 14:06:28 +0200

    Fix warning: Enumerator.new without a block is deprecated; use Object#to_enum

--- lib/dynamoid/criteria/chain.rb
+++ lib/dynamoid/criteria/chain.rb
@@ -170,7 +170,7 @@ module Dynamoid #:nodoc:
       def records_with_index
         ids = ids_from_index
         if ids.nil? || ids.empty?
-          Enumerator.new []
+          [].to_enum
         else
           ids = ids.to_a
 
