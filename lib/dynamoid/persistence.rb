78e9d5a5ba2f0cb228e5a86d941cef963b49f765
Brenden Kokoszka <brenden.kokoszka@gmail.com>
2016-01-27 23:51:03 -0800

    Don't prefix table names when namespace is nil

--- lib/dynamoid/persistence.rb
+++ lib/dynamoid/persistence.rb
@@ -16,7 +16,15 @@ module Dynamoid
     module ClassMethods
 
       def table_name
-        @table_name ||= "#{Dynamoid::Config.namespace}_#{options[:name] || base_class.name.split('::').last.downcase.pluralize}"
+        table_base_name = options[:name] || base_class.name.split('::').last
+          .downcase.pluralize
+        table_prefix = if Dynamoid::Config.namespace.nil? then
+          ''
+        else
+          "#{Dynamoid::Config.namespace}_"
+        end
+
+        @table_name ||= "#{table_prefix}#{table_base_name}"
       end
 
       # Creates a table.
