55a35d38088ca2940be5dd4efa2b20f79f97c7ed
Nigel Brookes-Thomas <nigel.brookes@gmail.com>
2016-05-20 13:57:44 +0100

    Some of the test cases cause a bit of pollution such as putting non-viable models in Dynamoid.included_models and creating tables outside of the namespace so we need to explicitly tear down any database that's been created.

--- spec/dynamoid/tasks/database_spec.rb
+++ spec/dynamoid/tasks/database_spec.rb
@@ -11,21 +11,34 @@ describe Dynamoid::Tasks::Database do
   end
 
   describe '#create_tables' do
-    context 'when the tables don\'t already exist' do
+    before(:each) do
+      Dynamoid.adapter.clear_cache!
+      # depending on test execution order, Dynamoid.included_models gets polluted
+      # so find everything that is capable of having a table_name
+      @models = Dynamoid.included_models.select{ |m| not m.base_class.try(:name).blank? }.uniq(&:table_name)
+      # depending on test execution order, there are some tables hanging about
+      # that are not in Dynamoid's table namespace and don't get auto cleaned.
+      # We need this gone.
+      existing_tables = @models.map{ |m| m.table_name } & Dynamoid.adapter.list_tables
+      existing_tables.each{ |t| Dynamoid.adapter.delete_table t }
+    end
+
+    context "when the tables don't already exist" do
       it 'should create tables' do
-        expect(Dynamoid.adapter.list_tables).not_to include( *Dynamoid.included_models.map{ |m| m.table_name } )
+        expect(Dynamoid.adapter.list_tables).not_to include( *@models.map{ |m| m.table_name } )
         results = Dynamoid::Tasks::Database.create_tables
-        expect(Dynamoid.adapter.list_tables).to include( *Dynamoid.included_models.map{ |m| m.table_name } )
-        expect(results[:created]).to include( *Dynamoid.included_models.map{ |m| m.table_name } )
+        expect(Dynamoid.adapter.list_tables).to include( *@models.map{ |m| m.table_name } )
+        expect(results[:created]).to include( *@models.map{ |m| m.table_name } )
       end
     end
+
     context 'when the tables already exist' do
       it 'should not attempt to re-create the table' do
-        Address.create_table
-        expect(Dynamoid.adapter.list_tables).to include( Address.table_name )
+        User.create_table
+        expect(Dynamoid.adapter.list_tables).to include( User.table_name )
         results = Dynamoid::Tasks::Database.create_tables
-        expect(results[:existing]).to include( Address.table_name )
-        expect(results[:created]).not_to include( Address.table_name )
+        expect(results[:existing]).to include( User.table_name )
+        expect(results[:created]).not_to include( User.table_name )
       end
     end
   end
