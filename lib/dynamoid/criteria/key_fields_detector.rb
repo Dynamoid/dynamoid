e6a132ba4eddfbe882027fb7e749bc37ffb0c2e5
Andrew Konchin <andry.konchin@gmail.com>
2019-06-10 23:36:19 +0300

    KeyFieldsDetector. Move query-related logic to own Query class

--- lib/dynamoid/criteria/key_fields_detector.rb
+++ lib/dynamoid/criteria/key_fields_detector.rb
@@ -3,11 +3,30 @@
 module Dynamoid #:nodoc:
   module Criteria
     class KeyFieldsDetector
+
+      class Query
+        def initialize(query_hash)
+          @query_hash = query_hash
+          @fields_with_operator = query_hash.keys.map(&:to_s)
+          @fields = query_hash.keys.map(&:to_s).map { |s| s.split('.').first }
+        end
+
+        def contain?(field_name)
+          @fields.include?(field_name.to_s)
+        end
+
+        def contain_with_eq_operator?(field_name)
+          @fields_with_operator.include?(field_name.to_s)
+        end
+      end
+
+
       attr_reader :hash_key, :range_key, :index_name
 
       def initialize(query, source)
         @query = query
         @source = source
+        @query = Query.new(query)
 
         detect_keys
       end
@@ -19,14 +38,12 @@ module Dynamoid #:nodoc:
       private
 
       def detect_keys
-        query_keys = @query.keys.collect { |k| k.to_s.split('.').first }
-
         # See if querying based on table hash key
-        if @query.keys.map(&:to_s).include?(@source.hash_key.to_s)
+        if @query.contain_with_eq_operator?(@source.hash_key)
           @hash_key = @source.hash_key
 
           # Use table's default range key
-          if query_keys.include?(@source.range_key.to_s)
+          if @query.contain?(@source.range_key)
             @range_key = @source.range_key
             return
           end
@@ -34,7 +51,7 @@ module Dynamoid #:nodoc:
           # See if can use any local secondary index range key
           # Chooses the first LSI found that can be utilized for the query
           @source.local_secondary_indexes.each do |_, lsi|
-            next unless query_keys.include?(lsi.range_key.to_s)
+            next unless @query.contain?(lsi.range_key)
 
             @range_key = lsi.range_key
             @index_name = lsi.name
@@ -49,8 +66,8 @@ module Dynamoid #:nodoc:
         # But only do so if projects ALL attributes otherwise we won't
         # get back full data
         @source.global_secondary_indexes.each do |_, gsi|
-          next unless @query.keys.map(&:to_s).include?(gsi.hash_key.to_s) && gsi.projected_attributes == :all
-          next if @hash_key.present? && !query_keys.include?(gsi.range_key.to_s)
+          next unless @query.contain_with_eq_operator?(gsi.hash_key) && gsi.projected_attributes == :all
+          next if @hash_key.present? && !@query.contain?(gsi.range_key)
 
           @hash_key = gsi.hash_key
           @range_key = gsi.range_key
