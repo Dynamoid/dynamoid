f1517906d4be624ed7afe1395c8e6fc9a92a0c95
Andrew Konchin <andry.konchin@gmail.com>
2019-06-11 01:44:13 +0300

    KeyFieldsDetector. Refactor detecting logic

--- lib/dynamoid/criteria/key_fields_detector.rb
+++ lib/dynamoid/criteria/key_fields_detector.rb
@@ -20,58 +20,109 @@ module Dynamoid #:nodoc:
         end
       end
 
-
-      attr_reader :hash_key, :range_key, :index_name
-
       def initialize(query, source)
         @query = query
         @source = source
         @query = Query.new(query)
-
-        detect_keys
+        @result = find_keys_in_query
       end
 
       def key_present?
-        @hash_key.present?
+        @result.present?
+      end
+
+      def hash_key
+        @result && @result[:hash_key]
+      end
+
+      def range_key
+        @result && @result[:range_key]
+      end
+
+      def index_name
+        @result && @result[:index_name]
       end
 
       private
 
-      def detect_keys
-        # See if querying based on table hash key
-        if @query.contain_with_eq_operator?(@source.hash_key)
-          @hash_key = @source.hash_key
+      def find_keys_in_query
+        match_table_and_sort_key ||
+          match_local_secondary_index ||
+          match_global_secondary_index_and_sort_key ||
+          match_table ||
+          match_global_secondary_index
+      end
 
-          # Use table's default range key
-          if @query.contain?(@source.range_key)
-            @range_key = @source.range_key
-            return
-          end
+      # Use table's default range key
+      def match_table_and_sort_key
+        return unless @query.contain_with_eq_operator?(@source.hash_key)
+        return unless @source.range_key
 
-          # See if can use any local secondary index range key
-          # Chooses the first LSI found that can be utilized for the query
-          @source.local_secondary_indexes.each do |_, lsi|
-            next unless @query.contain?(lsi.range_key)
+        if @query.contain?(@source.range_key)
+          {
+            hash_key: @source.hash_key,
+            range_key: @source.range_key
+          }
+        end
+      end
 
-            @range_key = lsi.range_key
-            @index_name = lsi.name
-          end
+      # See if can use any local secondary index range key
+      # Chooses the first LSI found that can be utilized for the query
+      def match_local_secondary_index
+        return unless @query.contain_with_eq_operator?(@source.hash_key)
 
-          return if @range_key.present?
+        lsi = @source.local_secondary_indexes.values.find do |lsi|
+          @query.contain?(lsi.range_key)
         end
 
-        # See if can use any global secondary index
-        # Chooses the last GSI found that can be utilized for the query
-        # GSI with range key used in query has higher priority
-        # But only do so if projects ALL attributes otherwise we won't
-        # get back full data
-        @source.global_secondary_indexes.each do |_, gsi|
-          next unless @query.contain_with_eq_operator?(gsi.hash_key) && gsi.projected_attributes == :all
-          next if @hash_key.present? && !@query.contain?(gsi.range_key)
+        if lsi.present?
+          {
+            hash_key: @source.hash_key,
+            range_key: lsi.range_key,
+            index_name: lsi.name,
+          }
+        end
+      end
 
-          @hash_key = gsi.hash_key
-          @range_key = gsi.range_key
-          @index_name = gsi.name
+      # See if can use any global secondary index
+      # Chooses the first GSI found that can be utilized for the query
+      # GSI with range key involved into query conditions has higher priority
+      # But only do so if projects ALL attributes otherwise we won't
+      # get back full data
+      def match_global_secondary_index_and_sort_key
+        gsi = @source.global_secondary_indexes.values.find do |gsi|
+          @query.contain_with_eq_operator?(gsi.hash_key) && gsi.projected_attributes == :all &&
+            @query.contain?(gsi.range_key)
+        end
+
+        if gsi.present?
+          {
+            hash_key: gsi.hash_key,
+            range_key: gsi.range_key,
+            index_name: gsi.name,
+          }
+        end
+      end
+
+      def match_table
+        return unless @query.contain_with_eq_operator?(@source.hash_key)
+
+        {
+          hash_key: @source.hash_key,
+        }
+      end
+
+      def match_global_secondary_index
+        gsi = @source.global_secondary_indexes.values.find do |gsi|
+          @query.contain_with_eq_operator?(gsi.hash_key) && gsi.projected_attributes == :all
+        end
+
+        if gsi.present?
+          {
+            hash_key: gsi.hash_key,
+            range_key: gsi.range_key,
+            index_name: gsi.name,
+          }
         end
       end
     end
