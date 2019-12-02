fc3ed8e8a13e3dfc3ba0fac5e113918246d9f44c
Andrew Konchin <andry.konchin@gmail.com>
2019-12-02 23:11:32 +0200

    Fix specs: clear cached connection between specs

--- spec/dynamoid/config_spec.rb
+++ spec/dynamoid/config_spec.rb
@@ -2,9 +2,21 @@ require 'spec_helper'
 
 describe Dynamoid::Config do
   describe 'credentials' do
-    it 'passes credentials to a client connection', config: {
-      credentials: Aws::Credentials.new('your_access_key_id', 'your_secret_access_key')
-    } do
+    let(:credentials_new) do
+      Aws::Credentials.new('your_access_key_id', 'your_secret_access_key')
+    end
+
+    before do
+      @credentials_old, Dynamoid.config.credentials = Dynamoid.config.credentials, credentials_new
+      Dynamoid.adapter.connect!  # clear cached client
+    end
+
+    after do
+      Dynamoid.config.credentials = @credentials_old
+      Dynamoid.adapter.connect!  # clear cached client
+    end
+
+    it 'passes credentials to a client connection' do
       credentials = Dynamoid.adapter.client.config.credentials
 
       expect(credentials.access_key_id).to eq 'your_access_key_id'
