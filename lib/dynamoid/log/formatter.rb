5bad2f380473b7afa9fcd5999d90f599d8a9ae42
Andrew Konchin <andry.konchin@gmail.com>
2020-07-08 01:36:43 +0300

    Add debug log formatter

---
+++ lib/dynamoid/log/formatter.rb
@@ -0,0 +1,26 @@
+module Dynamoid
+  module Log
+    module Formatter
+
+      # https://docs.aws.amazon.com/sdk-for-ruby/v3/api/Aws/Log/Formatter.html
+      # https://docs.aws.amazon.com/sdk-for-ruby/v2/api/Seahorse/Client/Response.html
+      # https://aws.amazon.com/ru/blogs/developer/logging-requests/
+      class Debug
+        def format(response)
+          bold = "\x1b[1m"
+          color = "\x1b[34m"
+          reset = "\x1b[0m"
+
+          [
+            response.context.operation.name,
+            "#{bold}#{color}\nRequest:\n#{reset}#{bold}",
+            JSON.pretty_generate(JSON.parse(response.context.http_request.body.string)),
+            "#{bold}#{color}\nResponse:\n#{reset}#{bold}",
+            JSON.pretty_generate(JSON.parse(response.context.http_response.body.string)),
+            reset
+          ].join("\n")
+        end
+      end
+    end
+  end
+end
---
+++ spec/dynamoid/log/formatter/debug_spec.rb
@@ -0,0 +1,92 @@
+require 'spec_helper'
+require 'dynamoid/log/formatter'
+
+describe Dynamoid::Log::Formatter::Debug do
+  describe '#format' do
+    let(:subject) { described_class.new }
+
+    let(:logger) { Logger.new(buffer) }
+    let(:buffer) { StringIO.new }
+
+    let(:request) do
+      <<~JSON
+        {
+          "TableName": "dynamoid_tests_items",
+          "KeySchema": [
+            {
+              "AttributeName": "id",
+              "KeyType": "HASH"
+            }
+          ],
+          "AttributeDefinitions": [
+            {
+              "AttributeName": "id",
+              "AttributeType": "S"
+            }
+          ],
+          "BillingMode": "PROVISIONED",
+          "ProvisionedThroughput": {
+            "ReadCapacityUnits": 100,
+            "WriteCapacityUnits": 20
+          }
+        }
+      JSON
+    end
+
+    let(:response_pattern) do
+      Regexp.compile <<~JSON
+        \\{
+          "TableDescription": \\{
+            "AttributeDefinitions": \\[
+              \\{
+                "AttributeName": "id",
+                "AttributeType": "S"
+              \\}
+            \\],
+            "TableName": "dynamoid_tests_items",
+            "KeySchema": \\[
+              \\{
+                "AttributeName": "id",
+                "KeyType": "HASH"
+              \\}
+            \\],
+            "TableStatus": "ACTIVE",
+            "CreationDateTime": .+?,
+            "ProvisionedThroughput": \\{
+              "LastIncreaseDateTime": 0.0,
+              "LastDecreaseDateTime": 0.0,
+              "NumberOfDecreasesToday": 0,
+              "ReadCapacityUnits": 100,
+              "WriteCapacityUnits": 20
+            \\},
+            "TableSizeBytes": 0,
+            "ItemCount": 0,
+            "TableArn": ".+?"
+          \\}
+        \\}
+      JSON
+    end
+
+    before do
+      @log_formatter = Dynamoid.config.log_formatter
+      @logger = Dynamoid.config.logger
+
+      Dynamoid.config.log_formatter = subject
+      Dynamoid.config.logger = logger
+      Dynamoid.adapter.connect!  # clear cached client
+    end
+
+    after do
+      Dynamoid.config.log_formatter = @log_formatter
+      Dynamoid.config.logger = @logger
+      Dynamoid.adapter.connect!  # clear cached client
+    end
+
+    it 'logs request and response JSON body' do
+      new_class(table_name: 'items').create_table
+
+      expect(buffer.string).to include(request)
+      expect(buffer.string).to match(response_pattern)
+    end
+  end
+end
