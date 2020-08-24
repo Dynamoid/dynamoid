3f1353a12d5584b0ce3f6d2ddb624e9db3a16d10
beakal <beakal.teshome@coinbase.com>
2020-08-23 22:02:20 -0700

    add a test for adding a transaction

--- spec/dynamoid/persistence_spec.rb
+++ spec/dynamoid/persistence_spec.rb
@@ -1306,7 +1306,7 @@ describe Dynamoid::Persistence do
       obj = document_class.create(title: 'New Document')
 
       expect {
-        document_class.update_fields(obj.id, { title: 'New title', publisher: 'New publisher' } )
+        document_class.update_fields(obj.id, { title: 'New title', publisher: 'New publisher' })
       }.to raise_error Dynamoid::Errors::UnknownAttribute
     end
   end
@@ -1491,7 +1491,7 @@ describe Dynamoid::Persistence do
       obj = document_class.create(title: 'New Document')
 
       expect {
-        document_class.upsert(obj.id, { title: 'New title', publisher: 'New publisher' } )
+        document_class.upsert(obj.id, { title: 'New title', publisher: 'New publisher' })
       }.to raise_error Dynamoid::Errors::UnknownAttribute
     end
   end
@@ -2591,6 +2591,20 @@ describe Dynamoid::Persistence do
 
         expect { a1.destroy }.to_not raise_error
       end
+    end
+  end
+
+  describe '.transact' do
+    before do
+      Address.create_table
+    end
+
+    it 'process transaction' do
+      expect do
+        Address.transact([{
+                           put: { city: 'Chicago' }
+                         }, { put: { city: 'New York' } }])
+      end.to change { Address.count }.by(2)
     end
   end
 
