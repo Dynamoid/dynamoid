98eb893681ded36c53f9a5c170b9018b44517768
Andrew Konchin <andry.konchin@gmail.com>
2022-07-09 13:13:29 +0300

    Spec: don't use predefined model classes in loadable_spec.rb

--- spec/dynamoid/loadable_spec.rb
+++ spec/dynamoid/loadable_spec.rb
@@ -10,17 +10,36 @@ describe Dynamoid::Loadable do
     let(:tweet) { tweet = Tweet.create(tweet_id: 'x', group: 'abc') }
 
     it 'reflects persisted changes' do
+      klass = new_class do
+        field :city
+      end
+
+      address = klass.create(city: 'Miami')
+      copy = klass.find(address.id)
+
       address.update_attributes(city: 'Chicago')
-      expect(address.reload.city).to eq 'Chicago'
+      expect(copy.reload.city).to eq 'Chicago'
     end
 
-    it 'uses a :consistent_read' do
-      expect(Tweet).to receive(:find).with(tweet.hash_key, range_key: tweet.range_value, consistent_read: true).and_return(tweet)
+    it 'reads with strong consistency' do
+      klass = new_class do
+        field :message
+      end
+
+      tweet = klass.create
+
+      expect(klass).to receive(:find).with(tweet.id, consistent_read: true).and_return(tweet)
       tweet.reload
     end
 
     it 'works with range key' do
-      expect(tweet.reload.group).to eq 'abc'
+      klass = new_class do
+        field :message
+        range :group
+      end
+
+      tweet = klass.create(group: 'tech')
+      expect(tweet.reload.group).to eq 'tech'
     end
 
     it 'uses dumped value of sort key to load document' do
@@ -33,9 +52,7 @@ describe Dynamoid::Loadable do
       obj2 = klass.where(id: obj.id, activated_on: obj.activated_on).first
       obj2.update_attributes(name: 'New value')
 
-      expect { obj.reload }.to change {
-        obj.name
-      }.from('Old value').to('New value')
+      expect { obj.reload }.to change { obj.name }.from('Old value').to('New value')
     end
   end
 end
