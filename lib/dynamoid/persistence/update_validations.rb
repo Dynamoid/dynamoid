04fb8f8c854dccb9f6fd118be563d78fe9f1445f
Andrew Konchin <andry.konchin@gmail.com>
2025-02-27 22:30:39 +0200

    Construct UnknownAttribute error message uniformly

--- lib/dynamoid/persistence/update_validations.rb
+++ lib/dynamoid/persistence/update_validations.rb
@@ -7,9 +7,9 @@ module Dynamoid
       def self.validate_attributes_exist(model_class, attributes)
         model_attributes = model_class.attributes.keys
 
-        attributes.each_key do |attr_name|
-          unless model_attributes.include?(attr_name)
-            raise Dynamoid::Errors::UnknownAttribute, "Attribute #{attr_name} does not exist in #{model_class}"
+        attributes.each_key do |name|
+          unless model_attributes.include?(name)
+            raise Dynamoid::Errors::UnknownAttribute.new(model_class, name)
           end
         end
       end
