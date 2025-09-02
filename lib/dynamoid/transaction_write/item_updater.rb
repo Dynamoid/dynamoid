832fe37237806a9b2b95ce1bc5969603eb6950a1
Chris Hobbs <ckh@ckh>
2025-09-02 12:38:33 -0700

    In transaction writes, setting an attribute to nil should actually remove it RUBOCOP

--- lib/dynamoid/transaction_write/item_updater.rb
+++ lib/dynamoid/transaction_write/item_updater.rb
@@ -23,7 +23,7 @@ module Dynamoid
         if Dynamoid.config.store_attribute_with_nil_value
           @attributes_to_set.merge!(attributes)
         else
-          @attributes_to_set.merge!(attributes.select { |_, v| !v.nil? })
+          @attributes_to_set.merge!(attributes.reject { |_, v| v.nil? })
           @attributes_to_remove += attributes.select { |_, v| v.nil? }.keys
         end
       end
