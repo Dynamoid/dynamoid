eb7f6ff08ffa1ba993a2941ea50ebf2f3bda6195
Andrew Konchin <andry.konchin@gmail.com>
2025-10-04 01:55:41 +0300

    Implement class method delete()

--- spec/dynamoid/persistence/delete_spec.rb
+++ spec/dynamoid/persistence/delete_spec.rb
@@ -4,7 +4,157 @@ require 'spec_helper'
 require 'fixtures/persistence'
 
 RSpec.describe Dynamoid::Persistence do
-  describe 'delete' do
+  describe '.delete' do
+    let(:klass_with_composite_key) do
+      new_class do
+        range :age, :integer
+        field :name
+      end
+    end
+
+    it 'deletes an item' do
+      klass = new_class
+      obj = klass.create!
+
+      expect { klass.delete(obj.id) }.to change { klass.exists? obj.id }.from(true).to(false)
+    end
+
+    it 'deletes multiple items when given multiple partition keys' do
+      klass = new_class
+      obj1 = klass.create!
+      obj2 = klass.create!
+
+      expect {
+        expect {
+          klass.delete([obj1.id, obj2.id])
+        }.to change { klass.where(id: obj1.id).first }.to(nil)
+      }.to change { klass.where(id: obj2.id).first }.to(nil)
+    end
+
+    it 'deletes multiple items when given multiple partition and sort keys' do
+      obj1 = klass_with_composite_key.create!(age: 1)
+      obj2 = klass_with_composite_key.create!(age: 2)
+
+      expect {
+        expect {
+          klass_with_composite_key.delete([[obj1.id, obj1.age], [obj2.id, obj2.age]])
+        }.to change { klass_with_composite_key.where(id: obj1.id, age: obj1.age).first }.to(nil)
+      }.to change { klass_with_composite_key.where(id: obj2.id, age: obj2.age).first }.to(nil)
+    end
+
+    it 'uses dumped value of partition key to delete item' do
+      klass = new_class(partition_key: { name: :published_on, type: :date })
+
+      obj = klass.create!(published_on: '2018-10-07'.to_date)
+
+      expect {
+        klass.delete(obj.published_on)
+      }.to change { klass.where(published_on: obj.published_on).first }.to(nil)
+    end
+
+    it 'uses dumped value of partition key to delete item when given multiple primary keys' do
+      klass = new_class(partition_key: { name: :published_on, type: :date })
+
+      obj1 = klass.create!(published_on: '2018-10-07'.to_date)
+      obj2 = klass.create!(published_on: '2018-10-13'.to_date)
+
+      expect {
+        expect {
+          klass.delete([obj1.published_on, obj2.published_on])
+        }.to change { klass.where(published_on: obj1.published_on).first }.to(nil)
+      }.to change { klass.where(published_on: obj2.published_on).first }.to(nil)
+    end
+
+    it 'uses dumped value of sort key to delete item' do
+      klass = new_class do
+        range :activated_on, :date
+      end
+
+      obj = klass.create!(activated_on: Date.today)
+
+      expect {
+        klass.delete(obj.id, obj.activated_on)
+      }.to change { klass.where(id: obj.id, activated_on: obj.activated_on).first }.to(nil)
+    end
+
+    it 'uses dumped value of sort key to delete item when given multiple primary keys' do
+      klass = new_class do
+        range :activated_on, :date
+      end
+
+      obj1 = klass.create!(activated_on: Date.today)
+      obj2 = klass.create!(activated_on: Date.tomorrow)
+
+      expect {
+        expect {
+          klass.delete([[obj1.id, obj1.activated_on], [obj2.id, obj2.activated_on]])
+        }.to change { klass.where(id: obj1.id, activated_on: obj1.activated_on).first }.to(nil)
+      }.to change { klass.where(id: obj2.id, activated_on: obj2.activated_on).first }.to(nil)
+    end
+
+    it 'does not raise exception when model was concurrently deleted' do
+      klass = new_class
+      klass.create_table
+
+      expect(klass.delete('not-existing-id')).to eql(nil)
+    end
+
+    describe 'primary key validation' do
+      context 'simple primary key' do
+        it 'requires partition key to be specified' do
+          klass = new_class
+          expect { klass.delete(nil) }.to raise_exception(Dynamoid::Errors::MissingHashKey)
+          expect { klass.delete }.to raise_exception(Dynamoid::Errors::MissingHashKey)
+        end
+      end
+
+      context 'composite key' do
+        it 'requires partition key to be specified' do
+          expect { klass_with_composite_key.delete(nil, 1) }.to raise_exception(Dynamoid::Errors::MissingHashKey)
+        end
+
+        it 'requires partition key to be specified when given multiple primary keys' do
+          expect { klass_with_composite_key.delete([nil]) }.to raise_exception(Dynamoid::Errors::MissingHashKey)
+        end
+
+        it 'requires sort key to be specified' do
+          expect { klass_with_composite_key.delete('abc', nil) }.to raise_exception(Dynamoid::Errors::MissingRangeKey)
+          expect { klass_with_composite_key.delete('abc') }.to raise_exception(Dynamoid::Errors::MissingRangeKey)
+        end
+
+        it 'requires sort key to be specified when given multiple primary keys' do
+          expect { klass_with_composite_key.delete([['abc', nil]]) }.to raise_exception(Dynamoid::Errors::MissingRangeKey)
+          expect { klass_with_composite_key.delete([['abc']]) }.to raise_exception(Dynamoid::Errors::MissingRangeKey)
+        end
+      end
+    end
+
+    context 'when table arn is specified', remove_constants: [:Payment] do
+      it 'uses given table ARN in requests instead of a table name', config: { create_table_on_save: false } do
+        # Create table manually because CreateTable doesn't accept ARN as a
+        # table name. Add namespace to have this table removed automativally.
+        table_name = :"#{Dynamoid::Config.namespace}_purchases"
+        Dynamoid.adapter.create_table(table_name, :id)
+
+        table = Dynamoid.adapter.describe_table(table_name)
+        expect(table.arn).to be_present
+
+        Payment = Class.new do # rubocop:disable Lint/ConstantDefinitionInBlock, RSpec/LeakyConstantDeclaration
+          include Dynamoid::Document
+
+          table arn: table.arn
+        end
+
+        payment = Payment.create!
+
+        expect {
+          Payment.delete(payment.id)
+        }.to send_request_matching(:DeleteItem, { TableName: table.arn })
+      end
+    end
+  end
+
+  describe '#delete' do
     let(:klass_with_composite_key) do
       new_class do
         range :age, :integer
